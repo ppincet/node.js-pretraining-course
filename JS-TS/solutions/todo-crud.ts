e558de522f9955766e9555ac7b77e5587174d384
ppincet <7959215@gmail.com>
2025-11-06 09:38:16 +0300

    updates

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,25 @@
 import { Todo } from './types';
+ 
+function addTodo(state: Todo[], todo: Todo): Todo[] {
+  //throw new Error('addTodo: not implemented');
+  let result : Todo[] = [...state];
+  result.push(todo);
+  return result
 
-export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
 }
 
-export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
+function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
   throw new Error('updateTodo: not implemented');
 }
 
-export function removeTodo(state: Todo[], id: number): Todo[] {
+function removeTodo(state: Todo[], id: number): Todo[] {
+  return [...state].splice(state.findIndex(todo => todo.id == id), 1);
   throw new Error('removeTodo: not implemented');
 }
 
-export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+function getTodo(state: Todo[], id: number): Todo | undefined {
+  return [...state].find(todo => todo.id == id);
+  //throw new Error('getTodo: not implemented');
 }
+
+export { addTodo,updateTodo, removeTodo, getTodo }
--- utests/factory.ts
+++ utests/factory.ts
@@ -2,5 +2,5 @@ import { createTodo } from '../JS-TS/solutions/todo-factory';
 const a = createTodo({ title: 'Learn TypeScript', description: '' });
 const b = createTodo({ title: 'Refactor code' });
 
-console.log(a.id); // 1
-console.log(b.id); // 2
+console.log(`${a.id} : ${a.createdAt}`); // 1
+console.log(`${b.id} : ${b.createdAt}`); // 2
