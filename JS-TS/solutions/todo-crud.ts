4a5c97f5d46ee10651e2814defef2c6789cfbafe
ppincet <7959215@gmail.com>
2025-11-13 15:59:35 +0300

    todo-crud finished

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,3 +1,4 @@
+import { todo } from 'node:test';
 import { Todo } from './types';
 
 const addTodo = (state: Todo[], todo: Todo): Todo[] => {
@@ -7,17 +8,23 @@ const addTodo = (state: Todo[], todo: Todo): Todo[] => {
 }
 
 function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+  //throw new Error('updateTodo: not implemented');
+  return state.map(todo => {
+    if(todo.id == id) {
+      return {...todo, ...update };
+    } 
+    return todo;
+  });
 }
 
 const removeTodo = (state: Todo[], id: number): Todo[] => {
   //throw new Error('removeTodo: not implemented');
-  return [...state].splice(state.findIndex(todo => todo.id == id), 1);
+  return state.filter(todo => todo.id != id);
 }
 
 const getTodo = (state: Todo[], id: number): Todo | undefined => {
   //throw new Error('getTodo: not implemented');
-  return [...state].find(todo => todo.id == id);
+  return state.find(todo => todo.id == id);
 }
 
 export { addTodo,updateTodo, removeTodo, getTodo }
--- utests/factory-crud.ts
+++ utests/factory-crud.ts
@@ -4,5 +4,7 @@ import { TodoStatus, Todo } from '../JS-TS/solutions/types';
 let state : Todo[] = [];
 const state1 : Todo[] = addTodo(state, createTodo({ title: 'Write tests' }))
 const state2 = addTodo(state1, createTodo({ title: 'Update tests' }));
-console.log(state2);
+const state3 = removeTodo(state2, 1);
+console.log(state3);
+console.log(getTodo(state2, 100));
 //const state3 = updateTodo(state2, todo.id, { status: TodoStatus.COMPLETED });
